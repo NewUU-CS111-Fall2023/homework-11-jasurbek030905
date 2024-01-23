93d4425e1b692fd3b98b728cb67a67f821c7f72d
Jasurbek <148448603+jasurbek030905@users.noreply.github.com>
2024-01-23 23:36:52 +0500

    Update task_3.cpp

--- src/cpp/task_3.cpp
+++ src/cpp/task_3.cpp
@@ -1,8 +1,31 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
 
-class Problem3 {
-};
+using namespace std;
+
+bool word_break(string s, unordered_set<string>& wordDict) {
+    int n = s.length();
+    vector<bool> dp(n + 1, false);
+    dp[0] = true;  // Base case: empty string can be segmented
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (dp[j] && wordDict.count(s.substr(j, i - j))) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    return dp[n];
+}
+
+int main() {
+    string s = "newuzbekistan";
+    unordered_set<string> wordDict = {"new", "uzbekistan"};
+    bool can_segment = word_break(s, wordDict);
+    cout << "Can be segmented? " << can_segment << endl;
+    return 0;
+}
