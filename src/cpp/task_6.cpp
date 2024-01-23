8f45e1e3bd50027a2c757dbb726a0401d6a62100
Jasurbek <148448603+jasurbek030905@users.noreply.github.com>
2024-01-23 23:40:12 +0500

    Update task_6.cpp

--- src/cpp/task_6.cpp
+++ src/cpp/task_6.cpp
@@ -1,9 +1,34 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
 
-class Problem6 {
-public:
-};
+using namespace std;
+
+bool is_prime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void generate_primes(int n) {
+    int count = 0;
+    int num = n + 1;
+    while (count < 2) {
+        if (is_prime(num)) {
+            cout << num << " ";
+            count++;
+        }
+        num++;
+    }
+    cout << endl;
+}
+
+int main() {
+    int n = 10;
+    generate_primes(n);
+    return 0;
+}
