393665886c00b7e6843f5cd439bc195f891e9b3d
Jasurbek <148448603+jasurbek030905@users.noreply.github.com>
2024-01-23 23:41:56 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -1,9 +1,18 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
 
-class Problem7 {
-public:
-};
+int phi(int n) {
+    if (n == 1) return 0;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) return phi(n / i) * (i - 1);
+    }
+    return n - 1;
+}
+
+int main() {
+    int a = 13;
+    int b = 17;
+    int n = a * b;
+    int phi_n = phi(n);
+    std::cout << "phi(" << n << ") = " << phi_n << std::endl;
+    return 0;
+}
