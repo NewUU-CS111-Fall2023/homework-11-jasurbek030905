d769513e5dc2e249c6d659b02b4870e9f3e3d8a7
Jasurbek <148448603+jasurbek030905@users.noreply.github.com>
2024-01-23 23:39:14 +0500

    Update task_5.cpp

--- src/cpp/task_5.cpp
+++ src/cpp/task_5.cpp
@@ -1,8 +1,41 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <set>
 
-class Problem5 {
-};
+using namespace std;
+
+string decode_message(string key, string message) {
+    unordered_map<char, char> mapping;
+    set<char> seen_letters;
+
+
+    for (int i = 0; i < key.length(); i++) {
+        char ch = key[i];
+        if (isalpha(ch) && !seen_letters.count(ch)) {
+            mapping[ch] = 'a' + seen_letters.size();
+            seen_letters.insert(ch);
+        }
+    }
+
+
+    string decoded_message = "";
+    for (char ch : message) {
+        if (mapping.count(ch)) {
+            decoded_message += mapping[ch];
+        } else {
+            decoded_message += ch;
+        }
+    }
+
+    return decoded_message;
+}
+
+int main() {
+    string key = "happy boy";
+    string message = "lgb qv ph zsm";
+
+    string decoded_message = decode_message(key, message);
+    cout << "Decoded message: " << decoded_message << endl;
+    return 0;
+}
